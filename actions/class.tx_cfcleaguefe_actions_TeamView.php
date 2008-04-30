506f3e75c82e47782ebfa05a3dd9590e82d1a9f3
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2008-04-30 15:12:35 +0000

    Class changed to IOC.

--- actions/class.tx_cfcleaguefe_actions_TeamView.php
+++ actions/class.tx_cfcleaguefe_actions_TeamView.php
@@ -23,76 +23,68 @@
 ***************************************************************/
 
 require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+tx_div::load('tx_rnbase_action_BaseIOC');
 
 require_once(t3lib_extMgm::extPath('cfc_league_fe') . 'util/class.tx_cfcleaguefe_util_ScopeController.php');
 
 /**
- * Controller für die Anzeige eines Spielplans
+ * Controller für die Anzeige eines Teams
  */
-class tx_cfcleaguefe_actions_TeamView {
+class tx_cfcleaguefe_actions_TeamView extends tx_rnbase_action_BaseIOC {
 
-  /**
-   *
-   */
-  function execute($parameters,$configurations){
+	/**
+	 * handle request
+	 *
+	 * @param arrayobject $parameters
+	 * @param tx_rnbase_configurations $configurations
+	 * @param arrayobject $viewData
+	 * @return string
+	 */
+	function handleRequest(&$parameters,&$configurations, &$viewData) {
 
-// t3lib_div::debug($parameters,'ac_matchtable');
+		$teams = array();
+		// Im Flexform kann direkt ein Team ausgwählt werden
+		$teamId = intval($configurations->get('teamviewTeam'));
+		if(!$teamId) {
+			// Alternativ ist eine Parameterübergabe möglich
+			$teamId = intval($parameters->offsetGet('teamId'));
+			// Wenn die TeamID über den Parameter übergeben wird, dann müssen wir sie aus den
+			// Keepvars entfernen. Sonst funktionieren die Links auf den Scope nicht mehr.
+			$configurations->_keepVars->offsetUnset('teamId');
+			$parameters->offsetUnset('teamId');
+		}
+		if($teamId <= 0) {
+			// Nix angegeben also über den Scope suchen
+			// Die Werte des aktuellen Scope ermitteln
+			$scopeArr = tx_cfcleaguefe_util_ScopeController::handleCurrentScope($parameters,$configurations);
+			$saisonUids = $scopeArr['SAISON_UIDS'];
+			$groupUids = $scopeArr['GROUP_UIDS'];
+			$club = $configurations->get('teamviewClub');
 
-    $teams = array();
-    // Im Flexform kann direkt ein Team ausgwählt werden
-    $teamId = intval($configurations->get('teamviewTeam'));
-    if(!$teamId) {
-      // Alternativ ist eine Parameterübergabe möglich
-      $teamId = intval($parameters->offsetGet('teamId'));
-      // Wenn die TeamID über den Parameter übergeben wird, dann müssen wir sie aus den 
-      // Keepvars entfernen. Sonst funktionieren die Links auf den Scope nicht mehr.
-      $configurations->_keepVars->offsetUnset('teamId');
-      $parameters->offsetUnset('teamId');
-    }
-    if($teamId <= 0) {
-      // Nix angegeben also über den Scope suchen
-      // Die Werte des aktuellen Scope ermitteln
-      $scopeArr = tx_cfcleaguefe_util_ScopeController::handleCurrentScope($parameters,$configurations);
-      $saisonUids = $scopeArr['SAISON_UIDS'];
-      $groupUids = $scopeArr['GROUP_UIDS'];
-//    $compUids = $scopeArr['COMP_UIDS'];
-//    $roundUid = $scopeArr['ROUND_UIDS'];
-      $club = $configurations->get('teamviewClub');
+			// Ohne Club können wir nichts zeigen
+			if(intval($club) == 0) {
+				return 'Error: No club defined.';
+			}
 
-      // Ohne Club können wir nichts zeigen
-      if(intval($club) == 0) {
-        return 'Error: No club defined.';
-      }
+			$className = tx_div::makeInstanceClassName('tx_cfcleaguefe_models_club');
+			$club = new $className($club);
+			$teams = $club->getTeams($saisonUids, $groupUids);
+		}
+		else {
+			$className = tx_div::makeInstanceClassName('tx_cfcleaguefe_models_team');
+			$team = new $className($teamId);
+			$teams[] = $team;
+		}
 
-      $className = tx_div::makeInstanceClassName('tx_cfcleaguefe_models_club');
-      $club = new $className($club);
-      $teams = $club->getTeams($saisonUids, $groupUids);
-    }
-    else {
-      $className = tx_div::makeInstanceClassName('tx_cfcleaguefe_models_team');
-      $team = new $className($teamId);
-      $teams[] = $team;
+		$viewData =& $configurations->getViewData();
+		// Wir zeigen immer nur das erste Team im Ergebnis, selbst wenn es durch Fehlkonfiguration
+		// mehrere sein sollten
+		$viewData->offsetSet('team', $teams[0]);
 
-//t3lib_div::debug($teams, 'ac_teamview');
-    }
-
-    $viewData =& $configurations->getViewData();
-    // Wir zeigen immer nur das erste Team im Ergebnis, selbst wenn es durch Fehlkonfiguration
-    // mehrere sein sollten
-    $viewData->offsetSet('team', $teams[0]);
-
-//t3lib_div::debug(strftime('%y',0), 'ac_MatchTable');
-
-
-    // View
-    $view = tx_div::makeInstance('tx_cfcleaguefe_views_TeamView');
-    $view->setTemplatePath($configurations->getTemplatePath());
-    // Das Template wird komplett angegeben
-    $view->setTemplateFile($configurations->get('teamviewTemplate'));
-    $out = $view->render('teamview', $configurations);
-
-    return $out;
-  }
+		return null;
+	}
+	function getTemplateName() {return 'teamview';}
+	function getViewClassName() { return 'tx_cfcleaguefe_views_TeamView'; }
 
 }
 
