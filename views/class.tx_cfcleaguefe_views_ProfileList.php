136041d0496864388b671f329bae98e7d0fc9baa
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2008-04-30 14:39:37 +0000

    View completly refactored

--- views/class.tx_cfcleaguefe_views_ProfileList.php
+++ views/class.tx_cfcleaguefe_views_ProfileList.php
@@ -32,135 +32,78 @@ tx_div::load('tx_rnbase_view_Base');
  * Viewklasse für die Anzeige eines Personenprofils
  */
 class tx_cfcleaguefe_views_ProfileList extends tx_rnbase_view_Base {
-  /**
-   * Erstellen des Frontend-Outputs
-   */
-  function render($view, &$configurations){
-    $this->_init($configurations);
-    $cObj =& $configurations->getCObj(0);
+	/**
+	 * Create fe output
+	 *
+	 * @param string $template
+	 * @param arrayobject $viewData
+	 * @param tx_rnbase_configurations $configurations
+	 * @param tx_rnbase_util_FormatUtil $formatter
+	 * @return string
+	 */
+	function createOutput($template, &$viewData, &$configurations, &$formatter) {
+		$cObj =& $configurations->getCObj(0);
+		$markerArray = array(); // Eventuell später für allgemeine Daten oder Labels
 
-    $templateCode = $cObj->fileResource($this->getTemplate($view,'.html'));
+		$pagerData = $viewData->offsetGet('pagerData');
+		$charPointer = $viewData->offsetGet('charpointer');
+		$subpartArray['###CHARBROWSER###'] = $this->_createPager(
+													$cObj->getSubpart($template,'###CHARBROWSER###'),
+													$markerArray,
+													$pagerData,
+													$charPointer ,$cObj, $configurations);
 
-    $template = $cObj->getSubpart($templateCode,'###PROFILE_LIST###');
+		$listCnt =& $viewData->offsetGet('listsize');
+		$profiles =& $viewData->offsetGet('profiles');
 
-    // Die ViewData bereitstellen
-    $viewData =& $configurations->getViewData();
+		$builderClass = tx_div::makeInstanceClassName('tx_rnbase_util_ListBuilder');
+		$listBuilder = new $builderClass();
+		$out = $listBuilder->render($profiles,
+						$viewData, $template, 'tx_cfcleaguefe_util_ProfileMarker',
+						'profilelist.profile.', 'PROFILE', $formatter);
 
+		// Zum Schluß das Haupttemplate zusammenstellen
+		$out = $cObj->substituteMarkerArrayCached($out, $markerArray, $subpartArray); //, $wrappedSubpartArray);
 
-    $profiles =& $viewData->offsetGet('profiles');
-    $pagerData =& $viewData->offsetGet('pagerData');
-    $pointer = $viewData->offsetGet('pointer');
-
-    $markerArray = array(); // Eventuell später für allgemeine Daten oder Labels
-    $subpartArray['###PAGE_BROWSER###'] = $this->_createPager(
-                                              $cObj->getSubpart($template,'###PAGE_BROWSER###'), 
-                                              $markerArray, 
-                                              $pagerData, 
-                                              $pointer ,$cObj, $configurations);
-
-    $subpartArray['###PROFILES###'] = $this->_createPlayer(
-                                              $cObj->getSubpart($template,'###PROFILES###'), 
-                                              $markerArray, 
-                                              $profiles, 
-                                              $cObj, $configurations);
-
-
-    // Zum Schluß das Haupttemplate zusammenstellen
-    $out = $cObj->substituteMarkerArrayCached($template, $markerArray, $subpartArray); //, $wrappedSubpartArray);
-
-//    $out = $this->_createView($template, $profile, $cObj, $configurations);
-
-//t3lib_div::debug($profiles  , 'view_profile');
-
-    return $out ;
-  }
+		return $out ;
+	}
 
 
-  function _createPlayer($template, $markerArray, &$profiles, &$cObj, &$configurations) {
-    $out = '';
-
-    $token = md5(microtime());
-    $this->linkProfile->label($token);
-    $emptyArr = array();
-    $noLink = array('','');
-    $profileTemplate = $cObj->getSubpart($template,'###PROFILE###');
-
-    $profileMarkerClass = tx_div::makeInstanceClassName('tx_cfcleaguefe_util_ProfileMarker');
-    $profileMarker = new $profileMarkerClass;
-
-
-    for($i = 0; $i < count($profiles); $i++) {
-      $profile = $profiles[$i];
-
-      $out .= $profileMarker->parseTemplate($profileTemplate, $profile, $this->formatter, 'profilelist.profile.', $this->linkProfile);
-    }
-
-//t3lib_div::debug($out1, 'view_profile');
-
-    if(count($profiles)) {
-      // Zum Schluß das Haupttemplate zusammenstellen
-      $subpartArray['###PROFILE###'] = $out;
-      $out = $cObj->substituteMarkerArrayCached($template, $markerArray, $subpartArray); //, $wrappedSubpartArray);
-    }
-    else { // Keine Spieler vorhanden, es wird ein leerer String gesendet
-      $out = '';
-    }
-    return $out;
-  }
-
-  /**
-   * Liefert den Pagerstring
-   * TODO: Hier müssen noch Formatierungen eingebaut werden!
-   */
-  function _createPager($template, $markerArray, &$pagerData, $curr_pointer , &$cObj, &$configurations) {
+	/**
+	 * Liefert den Pagerstring
+	 * TODO: Hier müssen noch Formatierungen eingebaut werden!
+	 */
+	function _createPager($template, $markerArray, &$pagerData, $curr_pointer , &$cObj, &$configurations) {
     $out = array();
-
-    $token = md5(microtime());
-    $this->linkList->label($token);
+		$link = $configurations->createLink(); // Link auf die eigene Seite
+		$link->destination($GLOBALS['TSFE']->id); // Das Ziel der Seite vorbereiten
+   	$token = md5(microtime());
+    $link->label($token);
     $emptyArr = array();
+		$pagerItems = $pagerData['list'];
+		if(!is_array($pagerItems) || !count($pagerItems)) return '';
+		
+		while(list($pointer, $size) = each($pagerItems)) {
+			$myMarkerArray = $markerArray;
+			$myMarkerArray['###PB_ITEM###'] = $pointer;
+			$myMarkerArray['###PB_ITEM_SIZE###'] = $size;
 
-    while(list($pointer, $size) = each($pagerData)) {
-      $myMarkerArray = $markerArray;
-      $myMarkerArray['###PB_ITEM###'] = $pointer;
-      $myMarkerArray['###PB_ITEM_SIZE###'] = $size;
+			if(strcmp($pointer, $curr_pointer)) {
+				$link->parameters(array('charpointer' => $pointer));
+				$wrappedSubpartArray['###PB_ITEM_LINK###'] = explode($token, $link->makeTag());
+			}
+			else
+				$wrappedSubpartArray['###PB_ITEM_LINK###'] = $emptyArr;
+			$out[] = $cObj->substituteMarkerArrayCached($template, $myMarkerArray, $emptyArray, $wrappedSubpartArray);
+		}
 
-//t3lib_div::debug($pointer . '!=' . $curr_pointer  , 'view_profile');
-
-      if(strcmp($pointer, $curr_pointer)) {
-        $this->linkList->parameters(array('pointer' => $pointer));
-        $wrappedSubpartArray['###PB_ITEM_LINK###'] = explode($token, $this->linkList->makeTag());
-      }
-      else
-        $wrappedSubpartArray['###PB_ITEM_LINK###'] = $emptyArr;
-//      $subpartArray['###PB_ITEM_LINK###']
-      $out[] = $cObj->substituteMarkerArrayCached($template, $myMarkerArray, $emptyArray, $wrappedSubpartArray);
-    }
-
-    return implode('-',$out);
+		return implode($configurations->get('profilelist.charbrowser.implode'),$out);
   }
 
-  function _init(&$configurations) {
-    $this->formatter = &$configurations->getFormatter();
-
-    $linkClass = tx_div::makeInstanceClassName('tx_lib_link');
-    $target = intval($configurations->get('profilelistPage'));
-    $this->linkProfile = new $linkClass;
-    $this->linkProfile->designatorString = $configurations->getQualifier();
-    $this->linkProfile->destination($target); // Das Ziel der Seite vorbereiten
-
-    $cObj = &$configurations->getCObj(0);
-    $target = $cObj->data['pid'];
-//t3lib_div::debug($cObj->data['pid'], 'view_profile');
-
-    $this->linkList = new $linkClass; // Link auf die eigene Seite
-    $this->linkList->designatorString = $configurations->getQualifier();
-    $this->linkList->destination($target); // Das Ziel der Seite vorbereiten
-
-  }
-
-//t3lib_div::debug($damPics['files'][603] , 'view_teamview');
-//t3lib_div::debug($media, 'view_teamview');
-
+	function getMainSubpart(&$viewData) {
+		return '###PROFILE_LIST###';
+	}
+  
 }
 
 
