fe0bd3fc28eb896401b249ddfd1409787c48eb66
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2010-10-10 11:01:23 +0000

    Avoid warning

--- actions/class.tx_cfcleaguefe_actions_TableChart.php
+++ actions/class.tx_cfcleaguefe_actions_TableChart.php
@@ -106,7 +106,7 @@ class tx_cfcleaguefe_actions_TableChart {
 		$this->createChartDataset($xyDataset, $tsArr, $configurations, $league);
 		try {
 			tx_rnbase::load('tx_rnbase_plot_Builder');
-			$chart = tx_rnbase_plot_Builder::getInstance()->make($tsArr);
+			$chart = tx_rnbase_plot_Builder::getInstance()->make($tsArr, false);
 //			require_once(PATH_site.t3lib_extMgm::siteRelPath('pbimagegraph').'class.tx_pbimagegraph_ts.php');
 //			$chart = tx_pbimagegraph_ts::make($tsArr);
 		}
