ce2cf57946c88f861e151f9f23c5763d772ff87e
digedag <rene@system25.de>
2013-02-02 16:00:49 +0100

    Wizicon fixed for TYPO3 6.0

--- ext_tables.php
+++ ext_tables.php
@@ -8,9 +8,8 @@ t3lib_div::loadTCA('tt_content');
  *  The plugins need to be included in ext_tables.php 
  */
 
-require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase_controller.php');
-
-//t3lib_div::debug($_EXTKEY,'Test');
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_rnbase_controller');
 
 ////////////////////////////////
 // Plugin Competition anmelden
@@ -35,25 +34,15 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist']['tx_cfcleaguefe_repo
 $TCA['tt_content']['types']['list']['subtypes_addlist']['tx_cfcleaguefe_report']='pi_flexform';
 //$TCA['tt_content']['types']['list']['subtypes_addlist']['tx_cfcleaguefe_report']='pi_flexform,imagewidth;;13, --palette--;LLL:EXT:cms/locallang_ttc.php:ALT.imgLinks;7, --palette--;LLL:EXT:cms/locallang_ttc.php:ALT.imgOptions;11,tx_hldamgallery_displaypage';
 
-// Die Felder für die Bildbearbeitung einblenden
-//$TCA['tt_content']['types']['list']['subtypes_addlist']['tx_cfcleaguefe_report']='imagewidth'
-//$TCA['tt_content']['types']['list']['subtypes_addlist']['tx_cfcleaguefe_report']='imagewidth;;13'
-//                        --palette--;LLL:EXT:cms/locallang_ttc.php:ALT.imgOptions;11';
-
-// Zeige Bilder auf Gallerieseite
-//t3lib_extMgm::addToAllTCAtypes('tt_content','tx_hldamgallery_displaypage','tx_cfcleaguefe_report','');
-//t3lib_extMgm::addToAllTCAtypes('tt_content','tx_hldamgallery_displaypage','image','after:tx_damttcontent_files');
-
-// Galleriemodus
-//t3lib_extMgm::addToAllTCAtypes('tt_content','tx_hldamgallery_displaymode','tx_cfcleaguefe_report','after:tx_hldamgallery_displaypage');
-//t3lib_extMgm::addToAllTCAtypes('tt_content','tx_hldamgallery_displaymode','textpic','after:tx_hldamgallery_displaypage');
-
 
 t3lib_extMgm::addPiFlexFormValue('tx_cfcleaguefe_report','FILE:EXT:'.$_EXTKEY.'/flexform_report.xml');
 t3lib_extMgm::addPlugin(Array('LLL:EXT:'.$_EXTKEY.'/locallang_db.php:plugin.report.label','tx_cfcleaguefe_report'));
 
 # Add plugin wizards
-if (TYPO3_MODE=='BE')	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_cfcleaguefe_util_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'util/class.tx_cfcleaguefe_util_wizicon.php';
+if (TYPO3_MODE=='BE')	{
+	tx_rnbase::load('tx_rnbase_util_Wizicon');
+	tx_rnbase_util_Wizicon::addWizicon('tx_cfcleaguefe_util_wizicon', t3lib_extMgm::extPath($_EXTKEY).'util/class.tx_cfcleaguefe_util_wizicon.php');
+}
 
 // list static templates in templates selection
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'T3sports');
--- util/class.tx_cfcleaguefe_util_wizicon.php
+++ util/class.tx_cfcleaguefe_util_wizicon.php
@@ -1,40 +1,62 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008-2013 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+tx_rnbase::load('tx_rnbase_util_Wizicon');
 
 /**
  * Class that adds the wizard icon.
  *
  * @author	René Nitzsche <rene[at]system25.de>
  */
-class tx_cfcleaguefe_util_wizicon {
+class tx_cfcleaguefe_util_wizicon extends tx_rnbase_util_Wizicon {
+	/**
+	 * 
+	 */
+	protected function getPluginData() {
+		$plugins = array();
+		$plugins['tx_cfcleaguefe_competition'] = array(
+			'icon'=>t3lib_extMgm::extRelPath('cfc_league_fe').'/ext_icon.gif',
+			'title'=>'plugin.competition.label',
+			'description'=>'plugin.competition.description',
+		);
+		$plugins['tx_cfcleaguefe_report'] = array(
+			'icon'=>t3lib_extMgm::extRelPath('cfc_league_fe').'/ext_icon.gif',
+			'title'=>'plugin.report.label',
+			'description'=>'plugin.report.description',
+		);
+		return $plugins;
+	}
+	protected function getLLFile() {
+		return t3lib_extMgm::extPath('cfc_league_fe').'locallang_db.xml';
+	}
 	/**
 	 * Adds the T3sports report plugin wizard icon
 	 *
 	 * @param array Input array with wizard items for plugins
 	 * @return array Modified input array, having the items for T3sports plugins added.
 	 */
-	function proc($wizardItems)	{
+	function proc____($wizardItems)	{
 		global $LANG;
 
 		$LL = $this->includeLocalLang();
@@ -55,8 +77,9 @@ class tx_cfcleaguefe_util_wizicon {
 
 		return $wizardItems;
 	}
-	function includeLocalLang()	{
+	function includeLocalLang_____()	{
 		$llFile = t3lib_extMgm::extPath('cfc_league_fe').'locallang_db.xml';
+		// FIXME: Ändern
 		$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
 		return $LOCAL_LANG;
 	}
