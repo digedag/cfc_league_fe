0d5ba0ee82caad8a0fb1d25c177473b8b00017ef
René Nitzsche <rene@system25.de>
2017-06-04 11:53:42 +0200

    fix #22 getMainSubpart

--- views/class.tx_cfcleaguefe_views_MatchReport.php
+++ views/class.tx_cfcleaguefe_views_MatchReport.php
@@ -1,60 +1,51 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2007-2016 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-
+ *  Copyright notice
+ *
+ *  (c) 2007-2017 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 tx_rnbase::load('tx_rnbase_view_Base');
-
 
 /**
  * Viewklasse für die Anzeige der Ligatabelle mit Hilfe eines HTML-Templates.
  */
-class tx_cfcleaguefe_views_MatchReport extends tx_rnbase_view_Base {
-  function getMainSubpart() {return '###MATCHREPORT###';}
+class tx_cfcleaguefe_views_MatchReport extends tx_rnbase_view_Base
+{
 
-  /**
-   * Erstellen des Frontend-Outputs
-   */
-	function createOutput($template, &$viewData, &$configurations, &$formatter){
-    $cObj =& $this->formatter->cObj;
-    $matchReport = $viewData->offsetGet('matchReport');
+    function getMainSubpart(&$viewData)
+    {
+        return '###MATCHREPORT###';
+    }
 
-    $home = $matchReport->match->getHome();
-    $guest = $matchReport->match->getGuest();
+    /**
+     * Erstellen des Frontend-Outputs
+     */
+    function createOutput($template, &$viewData, &$configurations, &$formatter)
+    {
+        $matchReport = $viewData->offsetGet('matchReport');
 
-    // Marker:
-    // HOME_, GUEST_
-    // MATCH_
+        $matchMarker = tx_rnbase::makeInstance('tx_cfcleaguefe_util_MatchMarker');
+        $match = $matchReport->getMatch();
+        $matchStr = $matchMarker->parseTemplate($template, $match, $formatter, 'matchreport.match.', 'MATCH');
 
-    $matchMarker = tx_rnbase::makeInstance('tx_cfcleaguefe_util_MatchMarker');
-    $match = $matchReport->getMatch();
-    $matchStr = $matchMarker->parseTemplate($template, $match, $formatter, 'matchreport.match.', 'MATCH');
-
-    return $matchStr;
-  }
+        return $matchStr;
+    }
 }
 
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/views/class.tx_cfcleaguefe_views_MatchReport.php'])	{
-  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/views/class.tx_cfcleaguefe_views_MatchReport.php']);
-}
-?>
