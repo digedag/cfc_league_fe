60d845704cfed0bc60c13af541b9bf35c0a7b89b
René Nitzsche <nitzsche@dmk-ebusiness.de>
2017-12-02 15:12:56 +0100

    Code style

--- actions/class.tx_cfcleaguefe_actions_ProfileList.php
+++ actions/class.tx_cfcleaguefe_actions_ProfileList.php
@@ -1,178 +1,192 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2007-2016 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-
+ *  Copyright notice
+ *
+ *  (c) 2007-2017 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 tx_rnbase::load('tx_rnbase_action_BaseIOC');
-
-
 
 /**
  * Controller für die Anzeige einer Personenliste
- * Die Liste wird sortiert nach Namen angezeigt. Dabei wird ein Pager verwendet, der für
+ * Die Liste wird sortiert nach Namen angezeigt.
+ * Dabei wird ein Pager verwendet, der für
  * jeden Buchstaben eine eigene Seite erstellt.
  */
-class tx_cfcleaguefe_actions_ProfileList extends tx_rnbase_action_BaseIOC {
+class tx_cfcleaguefe_actions_ProfileList extends tx_rnbase_action_BaseIOC
+{
 
-	/**
-	 * handle request
-	 *
-	 * @param arrayobject $parameters
-	 * @param tx_rnbase_configurations $configurations
-	 * @param arrayobject $viewData
-	 * @return string
-	 */
-	function handleRequest(&$parameters, &$configurations, &$viewData) {
-// Zunächst sollten wir die Anfangsbuchstaben ermitteln
-    $service = tx_cfcleaguefe_util_ServiceRegistry::getProfileService();
+    /**
+     * handle request
+     *
+     * @param arrayobject $parameters            
+     * @param tx_rnbase_configurations $configurations            
+     * @param arrayobject $viewData            
+     * @return string
+     */
+    protected function handleRequest(&$parameters, &$configurations, &$viewData)
+    {
+        // Zunächst sollten wir die Anfangsbuchstaben ermitteln
+        $service = tx_cfcleaguefe_util_ServiceRegistry::getProfileService();
+        
+        if ($configurations->get('profilelist.charbrowser')) {
+            $pagerData = $this->findPagerData($service, $configurations);
+            
+            $firstChar = $parameters->offsetGet('charpointer');
+            $firstChar = (strlen(trim($firstChar)) > 0) ? substr($firstChar, 0, 1) : $pagerData['default'];
+            $viewData->offsetSet('pagerData', $pagerData);
+            $viewData->offsetSet('charpointer', $firstChar);
+        }
+        
+        $fields = array();
+        $options = array(
+            'count' => 1
+        );
+        $this->initSearch($fields, $options, $parameters, $configurations, $firstChar);
+        $listSize = $service->search($fields, $options);
+        unset($options['count']);
+        // PageBrowser initialisieren
+        $pageBrowser = tx_rnbase::makeInstance('tx_rnbase_util_PageBrowser', 'profiles');
+        $pageSize = $this->getPageSize($parameters, $configurations);
+        // Wurde neu gesucht?
+        if ($parameters->offsetGet('plnewsearch')) {
+            $pageBrowser->setState(null, $listSize, $pageSize);
+            $configurations->removeKeepVar('plnewsearch');
+        } else {
+            $pageBrowser->setState($parameters, $listSize, $pageSize);
+        }
+        $limit = $pageBrowser->getState();
+        $options = array_merge($options, $limit);
+        $result = $service->search($fields, $options);
+        $viewData->offsetSet('profiles', $result);
+        $viewData->offsetSet('pagebrowser', $pageBrowser);
+        
+        return null;
+    }
 
-		if($configurations->get('profilelist.charbrowser')) {
-			$pagerData = $this->findPagerData($service, $configurations);
+    protected function initSearch(&$fields, &$options, &$parameters, &$configurations, $firstChar)
+    {
+        // ggf. die Konfiguration aus der TS-Config lesen
+        tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
+        tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profilelist.options.');
+        $timeRange = $configurations->get('profilelist.birthdays');
+        if ($timeRange) {
+            $timePattern = $timeRange == 'DAY' ? '%d%m' : '%m';
+            $where .= " ( (DATE_FORMAT(FROM_UNIXTIME(tx_cfcleague_profiles.birthday), '${timePattern}') = DATE_FORMAT(CURDATE(), '${timePattern}')";
+            $where .= ' AND tx_cfcleague_profiles.birthday > 0) OR ';
+            // Variante 2 für Zeiten vor 1970
+            $where .= " (DATE_FORMAT(SUBDATE('1970-01-01', DATEDIFF(FROM_UNIXTIME( ABS( tx_cfcleague_profiles.birthday )), '1970-01-01')), '${timePattern}') = DATE_FORMAT(CURDATE(), '${timePattern}')";
+            $where .= ' AND tx_cfcleague_profiles.birthday < 0 ))';
+            if ($fields[SEARCH_FIELD_CUSTOM])
+                $fields[SEARCH_FIELD_CUSTOM] .= ' AND ';
+            $fields[SEARCH_FIELD_CUSTOM] .= $where;
+            
+            // Sortierung nach Datum
+            $sort = array(
+                'DATE_FORMAT(FROM_UNIXTIME(tx_cfcleague_profiles.birthday), \'%m%d\')' => 'asc'
+            );
+            if (is_array($options['orderby']))
+                $options['orderby'] = array_merge($sort, $options['orderby']);
+            else
+                $options['orderby'] = $sort;
+        }
+        if ($firstChar) {
+            $specials = tx_rnbase_util_SearchBase::getSpecialChars();
+            $firsts = $specials[$firstChar];
+            if ($firsts) {
+                $firsts = implode('\',\'', $firsts);
+            } else
+                $firsts = $firstChar;
+            
+            if ($fields[SEARCH_FIELD_CUSTOM])
+                $fields[SEARCH_FIELD_CUSTOM] .= ' AND ';
+            $fields[SEARCH_FIELD_CUSTOM] .= "LEFT(UCASE(last_name),1) IN ('$firsts') ";
+            ;
+        }
+    }
 
-			$firstChar = $parameters->offsetGet('charpointer');
-			$firstChar = (strlen(trim($firstChar)) > 0) ? substr($firstChar, 0, 1) : $pagerData['default'];
-			$viewData->offsetSet('pagerData', $pagerData);
-			$viewData->offsetSet('charpointer', $firstChar);
-		}
+    /**
+     * Wir verwenden einen alphabetischen Pager.
+     * Also muß zunächst ermittelt werden, welche
+     * Buchstaben überhaupt vorkommen.
+     * 
+     * @param tx_cfcleaguefe_ProfileService $service            
+     * @param tx_rnbase_configurations $configurations            
+     */
+    protected function findPagerData($service, &$configurations)
+    {
+        $options = [];
+        $options['what'] = 'LEFT(UCASE(last_name),1) As first_char, count(LEFT(UCASE(last_name),1)) As size';
+        $options['groupby'] = 'LEFT(UCASE(last_name),1)';
+        $fields = array();
+        tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
+        tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profilelist.options.');
+        
+        $rows = $service->search($fields, $options);
+        
+        $specials = tx_rnbase_util_SearchBase::getSpecialChars();
+        $wSpecials = array();
+        foreach ($specials as $key => $special) {
+            foreach ($special as $char) {
+                $wSpecials[$char] = $key;
+            }
+        }
+        
+        $ret = array();
+        foreach ($rows as $row) {
+            if (array_key_exists(($row['first_char']), $wSpecials)) {
+                $ret[$wSpecials[$row['first_char']]] = intval($ret[$wSpecials[$row['first_char']]]) + $row['size'];
+            } else
+                $ret[$row['first_char']] = $row['size'];
+        }
+        
+        $current = 0;
+        if (count($ret)) {
+            $keys = array_keys($ret);
+            $current = $keys[0];
+        }
+        $data = [
+            'list' => $ret,
+            'default' => $current,
+        ];
+        return $data;
+    }
 
-		$fields = array();
-		$options = array('count'=> 1);
-		$this->initSearch($fields, $options, $parameters, $configurations, $firstChar);
-		$listSize = $service->search($fields, $options);
-		unset($options['count']);
-		// PageBrowser initialisieren
-		$pageBrowser = tx_rnbase::makeInstance('tx_rnbase_util_PageBrowser', 'profiles');
-		$pageSize = $this->getPageSize($parameters, $configurations);
-		//Wurde neu gesucht?
-		if($parameters->offsetGet('plnewsearch')) {
-			$pageBrowser->setState(null, $listSize, $pageSize);
-			$configurations->removeKeepVar('plnewsearch');
-		}
-		else {
-			$pageBrowser->setState($parameters, $listSize, $pageSize);
-		}
-		$limit = $pageBrowser->getState();
-		$options = array_merge($options, $limit);
-		$result = $service->search($fields, $options);
-		$viewData->offsetSet('profiles', $result);
-		$viewData->offsetSet('pagebrowser', $pageBrowser);
+    /**
+     * Liefert die Anzahl der Ergebnisse pro Seite
+     *
+     * @param array $parameters            
+     * @param tx_rnbase_configurations $configurations            
+     * @return int
+     */
+    protected function getPageSize(&$parameters, &$configurations)
+    {
+        return intval($configurations->get('profilelist.profile.pagebrowser.limit'));
+    }
 
-		return null;
-	}
-	protected function initSearch(&$fields, &$options, &$parameters, &$configurations, $firstChar) {
-		// ggf. die Konfiguration aus der TS-Config lesen
-		tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
-		tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profilelist.options.');
-		$timeRange = $configurations->get('profilelist.birthdays');
-		if($timeRange) {
-			$timePattern = $timeRange == 'DAY' ? '%d%m' : '%m';
-			$where .= " ( (DATE_FORMAT(FROM_UNIXTIME(tx_cfcleague_profiles.birthday), '${timePattern}') = DATE_FORMAT(CURDATE(), '${timePattern}')";
-			$where .= ' AND tx_cfcleague_profiles.birthday > 0) OR ';
-			// Variante 2 für Zeiten vor 1970
-			$where .= " (DATE_FORMAT(SUBDATE('1970-01-01', DATEDIFF(FROM_UNIXTIME( ABS( tx_cfcleague_profiles.birthday )), '1970-01-01')), '${timePattern}') = DATE_FORMAT(CURDATE(), '${timePattern}')";
-			$where .= ' AND tx_cfcleague_profiles.birthday < 0 ))';
-			if($fields[SEARCH_FIELD_CUSTOM]) $fields[SEARCH_FIELD_CUSTOM] .= ' AND ';
-			$fields[SEARCH_FIELD_CUSTOM] .= $where;
+    protected function getTemplateName()
+    {
+        return 'profilelist';
+    }
 
-			// Sortierung nach Datum
-			$sort = array('DATE_FORMAT(FROM_UNIXTIME(tx_cfcleague_profiles.birthday), \'%m%d\')' => 'asc');
-			if(is_array($options['orderby']))
-			  $options['orderby'] = array_merge($sort, $options['orderby']);
-			else
-				$options['orderby'] = $sort;
-		}
-		if($firstChar) {
-			$specials = tx_rnbase_util_SearchBase::getSpecialChars();
-			$firsts = $specials[$firstChar];
-			if($firsts) {
-				$firsts = implode('\',\'',$firsts);
-			}
-			else $firsts = $firstChar;
-
-			if($fields[SEARCH_FIELD_CUSTOM]) $fields[SEARCH_FIELD_CUSTOM] .= ' AND ';
-			$fields[SEARCH_FIELD_CUSTOM] .= "LEFT(UCASE(last_name),1) IN ('$firsts') ";;
-		}
-	}
-
-	/**
-	 * Wir verwenden einen alphabetischen Pager. Also muß zunächst ermittelt werden, welche
-	 * Buchstaben überhaupt vorkommen.
-	 * @param tx_cfcleaguefe_ProfileService $service
-	 * @param tx_rnbase_configurations $configurations
-	 */
-	function findPagerData(&$service, &$configurations) {
-
-		$options['what'] = 'LEFT(UCASE(last_name),1) As first_char, count(LEFT(UCASE(last_name),1)) As size';
-		$options['groupby'] = 'LEFT(UCASE(last_name),1)';
-		$fields = array();
-		tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
-		tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profilelist.options.');
-
-		$from = 'tx_cfcleague_profiles';
-		$rows = $service->search($fields, $options);
-
-		$specials = tx_rnbase_util_SearchBase::getSpecialChars();
-		$wSpecials = array();
-		foreach($specials As $key => $special) {
-			foreach ($special As $char) {
-				$wSpecials[$char] = $key;
-			}
-		}
-
-		$ret = array();
-		foreach($rows As $row) {
-			if(array_key_exists(($row['first_char']), $wSpecials)) {
-				$ret[$wSpecials[$row['first_char']]] = intval($ret[$wSpecials[$row['first_char']]]) + $row['size'];
-			}
-			else
-				$ret[$row['first_char']] = $row['size'];
-		}
-
-		$current = 0;
-		if(count($ret)) {
-			$keys = array_keys($ret);
-			$current = $keys[0];
-		}
-		$data['list'] = $ret;
-		$data['default'] = $current;
-		return $data;
-	}
-	/**
-	 * Liefert die Anzahl der Ergebnisse pro Seite
-	 *
-	 * @param array $parameters
-	 * @param tx_rnbase_configurations $configurations
-	 * @return int
-	 */
-	protected function getPageSize(&$parameters, &$configurations) {
-		return intval($configurations->get('profilelist.profile.pagebrowser.limit'));
-	}
-
-	function getTemplateName() {return 'profilelist';}
-	function getViewClassName() { return 'tx_cfcleaguefe_views_ProfileList'; }
+    protected function getViewClassName()
+    {
+        return 'tx_cfcleaguefe_views_ProfileList';
+    }
 }
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/actions/class.tx_cfcleaguefe_actions_ProfileList.php'])	{
-  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/actions/class.tx_cfcleaguefe_actions_ProfileList.php']);
-}
-
-?>
--- actions/class.tx_cfcleaguefe_actions_ProfileView.php
+++ actions/class.tx_cfcleaguefe_actions_ProfileView.php
@@ -1,88 +1,95 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2007-2016 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-
+ *  Copyright notice
+ *
+ *  (c) 2007-2017 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 tx_rnbase::load('tx_rnbase_action_BaseIOC');
 
 /**
  * Controller für die Anzeige eines Personenprofils
  */
-class tx_cfcleaguefe_actions_ProfileView extends tx_rnbase_action_BaseIOC {
-	static $exclude = array();
+class tx_cfcleaguefe_actions_ProfileView extends tx_rnbase_action_BaseIOC
+{
 
-	/**
-	 * handle request
-	 *
-	 * @param arrayobject $parameters
-	 * @param tx_rnbase_configurations $configurations
-	 * @param arrayobject $viewData
-	 * @return string
-	 */
-	function handleRequest(&$parameters, &$configurations, &$viewData) {
+    static $exclude = array();
 
-		$fields = array();
-		$options = array();
-		$this->initSearch($fields, $options, $parameters, $configurations);
+    /**
+     * handle request
+     *
+     * @param arrayobject $parameters            
+     * @param tx_rnbase_configurations $configurations            
+     * @param arrayobject $viewData            
+     * @return string
+     */
+    protected function handleRequest(&$parameters, &$configurations, &$viewData)
+    {
+        $fields = array();
+        $options = array();
+        $this->initSearch($fields, $options, $parameters, $configurations);
+        
+        $service = tx_cfcleaguefe_util_ServiceRegistry::getProfileService();
+        $profiles = $service->search($fields, $options);
+        $profile = count($profiles) ? $profiles[0] : null;
+        if (! $profile)
+            return 'No profile found!';
+        
+        $viewData->offsetSet('profile', $profile);
+        self::$exclude[] = $profile->uid;
+        return '';
+    }
 
-		$service = tx_cfcleaguefe_util_ServiceRegistry::getProfileService();
-		$profiles = $service->search($fields, $options);
-		$profile = count($profiles) ? $profiles[0] : null;
-		if(!$profile)
-			return 'No profile found!';
+    protected function initSearch(&$fields, &$options, $parameters, $configurations)
+    {
+        // ggf. die Konfiguration aus der TS-Config lesen
+        tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profileview.fields.');
+        tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profileview.options.');
+        
+        $options['limit'] = 1;
+        if (intval($configurations->get('profileview.excludeAlreadyDisplayed'))) {
+            // Doppelte Anzeige von Personen vermeiden
+            if (count(self::$exclude)) {
+                $fields['PROFILE.UID'][OP_NOTIN_INT] = implode(',', self::$exclude);
+            }
+        } else {
+            // Parameter prüfen
+            $value = $parameters->offsetGet('profileId');
+            if (intval($value)) {
+                $fields['PROFILE.UID'][OP_EQ_INT] = intval($value);
+            }
+        }
+    }
 
-		$viewData->offsetSet('profile', $profile);
-		self::$exclude[] = $profile->uid;
-		return $out;
-	}
+    public function getConfId()
+    {
+        return 'profileview.';
+    }
 
-	protected function initSearch(&$fields, &$options, $parameters, $configurations) {
-		// ggf. die Konfiguration aus der TS-Config lesen
-		tx_rnbase_util_SearchBase::setConfigFields($fields, $configurations, 'profileview.fields.');
-		tx_rnbase_util_SearchBase::setConfigOptions($options, $configurations, 'profileview.options.');
+    protected function getTemplateName()
+    {
+        return 'profile';
+    }
 
-		$options['limit'] = 1;
-		if(intval($configurations->get('profileview.excludeAlreadyDisplayed'))) {
-			// Doppelte Anzeige von Personen vermeiden
-			if(count(self::$exclude)) {
-				$fields['PROFILE.UID'][OP_NOTIN_INT] = implode(',', self::$exclude);
-			}
-		}
-		else {
-			// Parameter prüfen
-			$value = $parameters->offsetGet('profileId');
-			if(intval($value)) {
-				$fields['PROFILE.UID'][OP_EQ_INT] = intval($value);
-			}
-		}
-	}
-
-	public function getConfId() {return 'profileview.';}
-	function getTemplateName() {return 'profile';}
-	function getViewClassName() { return 'tx_cfcleaguefe_views_ProfileView'; }
+    protected function getViewClassName()
+    {
+        return 'tx_cfcleaguefe_views_ProfileView';
+    }
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/actions/class.tx_cfcleaguefe_actions_ProfileView.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league_fe/actions/class.tx_cfcleaguefe_actions_ProfileView.php']);
-}
-
-?>
