921e94e892b4b26d911e203f4c2560cb5c8f5ced
René Nitzsche <rene@system25.de>
2020-06-04 20:19:30 +0200

    #58 install possible, flexforms fixed

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+call_user_func(function()
+{
+    $extKey = 'cfc_league_fe';
+
+    // list static templates in templates selection
+    tx_rnbase_util_Extensions::addStaticFile($extKey,'static/', 'T3sports');
+    tx_rnbase_util_Extensions::addStaticFile($extKey,'static/cal/','T3sports cal-events');
+    tx_rnbase_util_Extensions::addStaticFile($extKey,'static/volleyball/','T3sports for Volleyball');
+});
--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,3 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 
-// list static templates in templates selection
-tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'static/', 'T3sports');
-tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'static/cal/','T3sports cal-events');
-tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'static/volleyball/','T3sports for Volleyball');
-
