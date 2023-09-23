ad434ae87ec1552ec4bf88e9539f37a6c60b54e7
René Nitzsche <rene@system25.de>
2023-09-23 21:40:10 +0200

    Fix profile list filter

--- Classes/Frontend/Action/ProfileList.php
+++ Classes/Frontend/Action/ProfileList.php
@@ -2,6 +2,8 @@
 
 namespace System25\T3sports\Frontend\Action;
 
+use InvalidArgumentException;
+use RuntimeException;
 use Sys25\RnBase\Configuration\ConfigurationInterface;
 use Sys25\RnBase\Frontend\Controller\AbstractAction;
 use Sys25\RnBase\Frontend\Filter\BaseFilter;
@@ -15,7 +17,7 @@ use System25\T3sports\Model\Repository\ProfileRepository;
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2007-2021 Rene Nitzsche (rene@system25.de)
+ *  (c) 2007-2023 Rene Nitzsche (rene@system25.de)
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -45,7 +47,7 @@ class ProfileList extends AbstractAction
 {
     private $profileRepo;
 
-    public function __construct(ProfileRepository $repo = null)
+    public function __construct(?ProfileRepository $repo = null)
     {
         $this->profileRepo = $repo ?: new ProfileRepository();
     }
@@ -58,12 +60,14 @@ class ProfileList extends AbstractAction
     protected function handleRequest(RequestInterface $request)
     {
         $configurations = $request->getConfigurations();
-        $parameters = $request->getParameters();
         $viewData = $request->getViewContext();
 
         $filter = BaseFilter::createFilter($request, $this->getConfId().'profile.');
         $fields = $options = [];
         $filter->init($fields, $options);
+
+        // FIXME: in Filterklasse auslagern
+        $this->initSearch($fields, $options, $configurations);
 
         // Soll ein CharBrowser verwendet werden
         $cbFilter = new CharBrowserFilter();
@@ -74,9 +78,6 @@ class ProfileList extends AbstractAction
             ],
             'colname' => 'last_name',
         ]);
-
-        // FIXME: in Filterklasse auslagern
-        $this->initSearch($fields, $options, $parameters, $configurations);
 
         // Soll ein PageBrowser verwendet werden
         $pbFilter = new PageBrowserFilter();
@@ -94,7 +95,17 @@ class ProfileList extends AbstractAction
         return null;
     }
 
-    protected function initSearch(&$fields, &$options, &$parameters, &$configurations)
+    /**
+     * @param array $fields
+     * @param array $options
+     * @param ConfigurationInterface $configurations
+     *
+     * @return void
+     *
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
+     */
+    protected function initSearch(&$fields, &$options, $configurations)
     {
         // ggf. die Konfiguration aus der TS-Config lesen
         SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
@@ -124,58 +135,6 @@ class ProfileList extends AbstractAction
                 $options['orderby'] = $sort;
             }
         }
-    }
-
-    /**
-     * Wir verwenden einen alphabetischen Pager.
-     * Also muß zunächst ermittelt werden, welche
-     * Buchstaben überhaupt vorkommen.
-     *
-     * @param \System25\T3sports\Service\ProfileService $service
-     * @param ConfigurationInterface $configurations
-     */
-    protected function findPagerData($service, ConfigurationInterface $configurations)
-    {
-        $options = [];
-        $options['what'] = 'LEFT(UCASE(last_name),1) As first_char, count(LEFT(UCASE(last_name),1)) As size';
-        $options['groupby'] = 'LEFT(UCASE(last_name),1)';
-        $fields = [];
-        SearchBase::setConfigFields($fields, $configurations, 'profilelist.fields.');
-        SearchBase::setConfigOptions($options, $configurations, 'profilelist.options.');
-        // replace orderby
-        unset($options['orderby']);
-        $options['orderby']['first_char'] = 'asc';
-
-        $rows = $service->search($fields, $options);
-
-        $specials = SearchBase::getSpecialChars();
-        $wSpecials = [];
-        foreach ($specials as $key => $special) {
-            foreach ($special as $char) {
-                $wSpecials[$char] = $key;
-            }
-        }
-
-        $ret = [];
-        foreach ($rows as $row) {
-            if (array_key_exists($row['first_char'], $wSpecials)) {
-                $ret[$wSpecials[$row['first_char']]] = intval($ret[$wSpecials[$row['first_char']]]) + $row['size'];
-            } else {
-                $ret[$row['first_char']] = $row['size'];
-            }
-        }
-
-        $current = 0;
-        if (count($ret)) {
-            $keys = array_keys($ret);
-            $current = $keys[0];
-        }
-        $data = [
-            'list' => $ret,
-            'default' => $current,
-        ];
-
-        return $data;
     }
 
     protected function getTemplateName()
